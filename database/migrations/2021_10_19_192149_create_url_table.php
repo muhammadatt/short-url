bd5f406b5d17014d49626aaca707003ae14299c5
Muhammad Tauhidi <matta08@gmail.com>
2021-10-20 11:14:21 -0400

    Updates to Url table migration

--- database/migrations/2021_10_19_192149_create_url_table.php
+++ database/migrations/2021_10_19_192149_create_url_table.php
@@ -15,8 +15,13 @@ class CreateUrlTable extends Migration
     {
         Schema::create('url', function (Blueprint $table) {
             $table->id();
-            $table->text('original');
-            $table->string('hash', 50);
+            //shortcode should be nullable because we'll need to save and generate an id before we generate the shortcode
+            $table->string('shortcode', 50)->nullable();
+            //the original url provided by user
+            $table->text('original')->nullable();
+            //flag to set whether the the link is Not Safe for Work
+            $table->boolean('nsfw')->default(0);
+            //counter for the number of times link has been clicked/viewed
             $table->unsignedInteger('view_count')->default(0);
             $table->timestamps();
         });
